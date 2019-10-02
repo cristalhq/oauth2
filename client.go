2db4c5e5052c38f3a54fb28181f18b1309621fcd
Oleg Kovalov <iamolegkovalov@gmail.com>
2019-10-02 18:16:04 +0200

    add Exchange method

--- client.go
+++ client.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 )
 
+// Client represents an OAuth2 HTTP client.
+//
 type Client struct {
 	client *http.Client
 	config *Config
@@ -11,10 +13,29 @@ type Client struct {
 	refreshToken string
 }
 
+// NewClient instantiates a new client with a given config.
+//
 func NewClient(client *http.Client, config *Config) *Client {
 	c := &Client{
 		client: client,
 		config: config,
 	}
 	return c
+}
+
+// Exchange converts an authorization code into an OAuth2 token.
+//
+func (c *Client) Exchange(ctx context.Context, code string) (*Token, error) {
+	v := url.Values{
+		"grant_type": []string{"authorization_code"},
+		"code":       []string{code},
+	}
+
+	if c.config.RedirectURL != "" {
+		v.Set("redirect_uri", c.config.RedirectURL)
+	}
+	return c.retrieveToken(ctx, v)
+}
+func (c *Client) retrieveToken(ctx context.Context, v url.Values) (*Token, error) {
+	return nil, nil
 }
