fb70241f530497eae62492b457562ab4a6abe8f4
Oleg Kovalov <iamolegkovalov@gmail.com>
2019-10-03 19:48:06 +0200

    implement token retrieval

--- client.go
+++ client.go
@@ -1,7 +1,18 @@
 package oauth2
 
 import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"mime"
 	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+	"time"
 )
 
 // Client represents an OAuth2 HTTP client.
@@ -37,5 +48,136 @@ func (c *Client) Exchange(ctx context.Context, code string) (*Token, error) {
 	return c.retrieveToken(ctx, v)
 }
 func (c *Client) retrieveToken(ctx context.Context, v url.Values) (*Token, error) {
-	return nil, nil
+	req, err := newTokenRequest(c.config.TokenURL, c.config.ClientID, c.config.ClientSecret, v, c.config.AuthStyle)
+	if err != nil {
+		return nil, err
+	}
+
+	token, err := c.sendRequest(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	return token, nil
+}
+
+func (c *Client) sendRequest(ctx context.Context, req *http.Request) (*Token, error) {
+	resp, err := c.client.Do(req.WithContext(ctx))
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := ioutil.ReadAll(io.LimitReader(resp.Body, 1<<20))
+	resp.Body.Close()
+
+	if err != nil {
+		return nil, fmt.Errorf("oauth2: cannot fetch token: %v", err)
+	}
+	if resp.StatusCode < 200 || resp.StatusCode > 299 {
+		return nil, fmt.Errorf("oauth2: cannot fetch token: %v %v\nResponse: %s",
+			resp.StatusCode, http.StatusText(resp.StatusCode), string(body))
+	}
+
+	var token *Token
+
+	switch responseContentType(resp) {
+	case "text/plain", "application/x-www-form-urlencoded":
+		token, err = parseText(body)
+	default:
+		token, err = parseJSON(body)
+	}
+
+	switch {
+	case err != nil:
+		return nil, err
+	case token.AccessToken == "":
+		return nil, errors.New("oauth2: server response missing access_token")
+	default:
+		return token, nil
+	}
+}
+
+func responseContentType(resp *http.Response) string {
+	content, _, _ := mime.ParseMediaType(resp.Header.Get("Content-Type"))
+	return content
+}
+
+func parseText(body []byte) (*Token, error) {
+	vals, err := url.ParseQuery(string(body))
+	if err != nil {
+		return nil, err
+	}
+
+	token := &Token{
+		AccessToken:  vals.Get("access_token"),
+		TokenType:    vals.Get("token_type"),
+		RefreshToken: vals.Get("refresh_token"),
+		raw:          vals,
+	}
+
+	e := vals.Get("expires_in")
+	expires, _ := strconv.Atoi(e)
+	if expires != 0 {
+		token.Expiry = time.Now().Add(time.Duration(expires) * time.Second)
+	}
+	return token, nil
+}
+
+func parseJSON(body []byte) (*Token, error) {
+	var tj tokenJSON
+	if err := json.Unmarshal(body, &tj); err != nil {
+		return nil, err
+	}
+
+	token := &Token{
+		AccessToken:  tj.AccessToken,
+		TokenType:    tj.TokenType,
+		RefreshToken: tj.RefreshToken,
+		Expiry:       tj.expiry(),
+		raw:          make(map[string]interface{}),
+	}
+
+	_ = json.Unmarshal(body, &token.raw) // no error checks for optional fields
+
+	return token, nil
+}
+
+func newTokenRequest(tokenURL, clientID, clientSecret string, v url.Values, authStyle AuthStyle) (*http.Request, error) {
+	needsAuthStyleProbe := authStyle == AuthStyleAutoDetect
+	if needsAuthStyleProbe {
+		authStyle = AuthStyleInHeader
+	}
+
+	if authStyle == AuthStyleInParams {
+		v = cloneURLValues(v)
+		if clientID != "" {
+			v.Set("client_id", clientID)
+		}
+		if clientSecret != "" {
+			v.Set("client_secret", clientSecret)
+		}
+	}
+
+	req, err := http.NewRequest("POST", tokenURL, strings.NewReader(v.Encode()))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	if authStyle == AuthStyleInHeader {
+		req.SetBasicAuth(url.QueryEscape(clientID), url.QueryEscape(clientSecret))
+	}
+	return req, nil
+}
+
+func cloneURLValues(vals url.Values) url.Values {
+	if vals == nil {
+		return url.Values{}
+	}
+
+	v2 := make(url.Values, len(vals))
+	for k, v := range vals {
+		v2[k] = append([]string(nil), v...)
+	}
+	return v2
 }
